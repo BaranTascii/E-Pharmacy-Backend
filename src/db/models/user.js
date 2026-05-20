b439829e5f04afdc34c2aa6797f32d4c1151ff73
Baran Taşcı <barantasci44@gmail.com>
2026-05-20 12:12:04 +0300

    create user and session mongoose models

---
+++ src/db/models/user.js
@@ -0,0 +1,26 @@
+import { model, Schema } from 'mongoose';
+
+const userSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: {
+      type: String,
+      enum: ['admin', 'user'],
+      default: 'user',
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
+
+userSchema.methods.toJSON = function () {
+  const obj = this.toObject();
+  delete obj.password;
+  return obj;
+};
+
+export const User = model('users', userSchema);
